26da87c9df9732e715e423567dc008f90a79ef25
abilarqamsayuri <144302495+abilarqamsayuri@users.noreply.github.com>
2023-10-12 11:32:24 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let teks = prompt('Merah Putih')
+
+// mengubah teks menjadi uppercase
+let uppercase = teks.toUpperCase()
+
+// mengubah teks menjadi lowcase
+let lowcase = teks.toLowerCase()
+
+// mengubah kata 'Putih' menjadi 'Muda'
+let ubahteks = teks.replace('Putih', 'Muda')
+
+// mengukur panjang (length) variable
+let panjangteks_length = teks.length
